6fb355635575bb2affa0ee79872dbe47c66054ad
kyleharrisnz <khar896@aucklanduni.ac.nz>
2026-09-04 21:21:40 +0000

    started testing for layer class

--- layer.py
+++ layer.py
@@ -7,4 +7,12 @@ class Layer(self, layer_count, num_inputs):
         # Create the layer of neurons with random weights and biases 
         for i in range(layer_count): 
             self.neurons.append(sigmoidNeuron(num_inputs))
-    
+
+
+if __name__ == "__main__": # Run this testing code when layer file called directly 
+    print("Hello, this is the direct file you are running from")
+    input = Layer(5, 2)
+    for i in input.nuerons.count(): 
+        print(i, input.nuerons[i])
+    pass
+ 
