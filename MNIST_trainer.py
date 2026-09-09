2c6174b3a59c3c45427836db15678fe3bf98808c
kyleharrisnz <khar896@aucklanduni.ac.nz>
2026-09-09 02:06:58 +0000

    Trained new models

--- MNIST_trainer.py
+++ MNIST_trainer.py
@@ -15,19 +15,13 @@ TRAINING_RATE2 = 0.11
 mnist = fetch_openml('mnist_784', version=1)
 #This is my neural network to recognise hand written numbers
 model1 = neuralNetwork.NeuralNetwork([HIDDEN_LAYER1_COUNT, HIDDEN_LAYER2_COUNT, OUTPUT_LAYER_COUNT])
-model1.load("trained_model3.npz")
-# for z in range(10): 
-#     for i in range(4000): 
-#         image = np.array(mnist.data.iloc[i])
-#         image = image/255
-#         target = mnist.target[i]
-#         number_detector1.train(image, target, TRAINING_RATE1)
-#         number_detector2.train(image, target, TRAINING_RATE2)
-for z in range(3): 
-    for i in range(70000):  
-        image = np.array(mnist.data.iloc[i])
+model1.load("trained_model5.npz")
+
+for z in range(7): 
+    for i in range(10000):  
+        image = np.array(mnist.data.iloc[i + 1000*z])
         image = image/255
-        target = mnist.target[i]
+        target = mnist.target[i+ 1000*z]
         model1.train(image, target, TRAINING_RATE1)
     
 print("Testing predictions of model with LEARNING_RATE1 = ", TRAINING_RATE1)
@@ -35,8 +29,8 @@ for i in range(60):
     test_image = np.array(mnist.data.iloc[i+10000]) / 255
     print("Target: ", mnist.target[i+10000], ", Prediction: ", model1.prediction(test_image))
 
-accuracy_model1 = neuralNetwork.accuracy(model1, 60000, 1000, mnist.data, mnist.target)
+accuracy_model1 = neuralNetwork.accuracy(model1, 30000, 10000, mnist.data, mnist.target)
 print("Model one Learning Rate: ", TRAINING_RATE1, " amount correct: ", accuracy_model1)
 
-model1.save("trained_model4")
+model1.save("trained_model6")
 
